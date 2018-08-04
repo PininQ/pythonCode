f826486ad2896d95f11cdbe0a46869e3cbec00b8
qinbin52qiul <2538323140@qq.com>
2018-08-04 10:59:56 +0800

    连接 MySQL 数据库

--- test_mysql/test_msyql.py
+++ test_mysql/test_msyql.py
@@ -1,7 +1,7 @@
 from datetime import date, datetime, timedelta
 import pymysql.cursors
 '''
-连接本地的 MySQL 数据库，需先在数据库中创建 testDB 数据库，执行如下语句向 testDB 中插入一条记录
+连接本地的 MySQL 数据库，需先在数据库中创建 testDB 数据库，执行如下语句向 testDB 数据库中插入一条记录
 '''
 # 连接配置信息
 config = {
